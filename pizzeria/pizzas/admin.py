200eb32ec66ee06e7249ae45573a073e07dc0890
Dmitrii Kilishek <mentalclear@gmail.com>
2021-08-21 11:28:18 -0400

    Completed exercise 18_4

--- pizzeria/pizzas/admin.py
+++ pizzeria/pizzas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Importing the models:
+from .models import Pizza, Topping
+
+admin.site.register(Pizza)
+admin.site.register(Topping)
