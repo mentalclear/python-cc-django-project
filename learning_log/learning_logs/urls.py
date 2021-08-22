5e3bd3b7c280c0d0036b1e9aa799f409018b720f
Dmitrii Kilishek <mentalclear@gmail.com>
2021-08-22 07:03:37 -0400

    Finished Ch18

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -5,7 +5,13 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns =[
-    # home page
-    path('', views.index, name="index")
+    # Home Page
+    path('', views.index, name="index"),
+    
+    # The page that shows all topics
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a signle topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
 
